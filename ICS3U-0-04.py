275b44b17e8e038c2ae3811cdf9bbbc5d55eacc4
amirmahdi801 <40166909+amirmahdi801@users.noreply.github.com>
2019-04-06 21:58:31 +0430

    fix: added comments, fixed string input error and changed the message for French language

--- ICS3U-0-04.py
+++ ICS3U-0-04.py
@@ -1,12 +1,24 @@
 print ("Welcome to my program")
 print ("please choose your language: 1.English 2.French 3.Spanish")
 language = input()
-language = int(language)
-if language == 1:
+if language == "1": # if the input was 1 print this message
     print ("hello my friend!")
-elif language == 2:
+elif language == "2": # if the input was 2 print this message
     print ("requescate de patche")
-elif language == 3:
+elif language == "3": # if the input was 3 print this message
     print ("Hola mi amigo")
-elif language > 3:
-    print ("wrong input!")
+else: # if the input was anything else than 1, 2 and 3 show this message
+    print ("wrong input")
+'''
+---------------------------------------
+-- Created by:     Amirmahdi Mersad  --
+-- Created on:     Apr 6 2019        --
+-- Created for:    Unit 0-04         --
+-- Course Code:    ICS3U             --
+-- Teacher Name:   Alireza Teimoori  --
+---------------------------------------
+-- this code sends a messange in 3   --
+-- languages and                     --
+---------------------------------------
+In this code, the user can choose his language and recieve a hi message :) from!
+'''
